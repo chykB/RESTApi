7ef23283d6867023e58cbd331ccec3d37ad3c183
chykaB <jodierhees@gmail.com>
2024-05-13 17:58:51 +0100

    Changes

--- apiProject/apiProject/urls.py
+++ apiProject/apiProject/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from api.views import BookListCreateAPIView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/books/', BookListCreateAPIView.as_view(), name='book-list-create')
 ]
